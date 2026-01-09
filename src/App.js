c4d7542ed679f06e855b77b4cfb9f82656d413b5
Dariusz Okoński <darek200180@gmail.com>
2026-01-09 06:42:35 +0100

    add App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import React, { Component } from 'react';
 import './App.css';
 import fruits from './foods';
 import { choice, remove } from './helpers';
 
-function App() {
-  return (
-    <div className='App App-header'>
-      <h1>Fruits app</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const fruit = choice(fruits);
+    const remaining = remove(fruits, fruit);
+
+    return (
+      <div className='App App-header'>
+        <h2>Fruits</h2>
+        <p>I would like one {fruit}, please</p>
+        <p>Here you go {fruit}</p>
+
+        <h3>Remaining</h3>
+        <p>
+          I am sorry, we're all out. We have only last {fruits.length} left:{' '}
+        </p>
+        <div>
+          {fruits.map((fruit) => (
+            <span key={Math.random()}>{fruit}</span>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
